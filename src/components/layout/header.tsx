be7d60e055ba2358818ef5fb0a7dd4761a6174fd
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2024-08-22 04:34:07 -0500

    make logo a like to navigate home

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,6 +3,8 @@ import { NavMenu } from '../ui/nav-menu';
 import { LogoutButton } from './logout-button';
 import { AccountTypeToggle } from './account-type-toggle';
 import { createClient } from '@/utils/supabase/server';
+import Link from 'next/link';
+import { NavLink } from '@/components/ui/link';
 
 export const Header = async ({ orgId }: { orgId?: string }) => {
 	const supabase = createClient();
@@ -12,7 +14,9 @@ export const Header = async ({ orgId }: { orgId?: string }) => {
 	return (
 		<header className="flex w-full items-center justify-between border-b border-b-input px-6 py-4">
 			<div className="flex items-center gap-3">
-				<div className="font-logo text-xl font-light">aveer.hr</div>
+				<NavLink org={orgId} href={'/'} className="font-logo text-xl font-light">
+					aveer.hr
+				</NavLink>
 				{data?.user && <AccountTypeToggle orgId={orgId} />}
 			</div>
 
