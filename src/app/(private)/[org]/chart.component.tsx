170dcb158dc0d524e3da676a4142d9f32b069751
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2024-08-22 20:02:03 -0500

    revert dashboard chart style

--- src/app/(private)/[org]/chart.component.tsx
+++ src/app/(private)/[org]/chart.component.tsx
@@ -8,25 +8,21 @@ export const DashboardCharts = async ({ org }: { org: string }) => {
 
 	return (
 		<>
-			<Card className="relative flex w-full max-w-72 flex-wrap items-start justify-between gap-6 p-4 before:absolute before:-left-px before:bottom-4 before:h-24 before:w-1 before:bg-foreground">
-				<div className="grid gap-2">
-					<h3 className="text-sm font-medium">People</h3>
-					<p className="text-8xl font-bold">
-						{data?.signed_contracts && data?.signed_contracts < 10 ? '0' : ''}
-						{data?.signed_contracts || 0}
-					</p>
-				</div>
-			</Card>
+			<div className="grid gap-2">
+				<h3 className="text-sm font-medium">People</h3>
+				<p className="text-8xl font-bold">
+					{data?.signed_contracts && data?.signed_contracts < 10 ? '0' : ''}
+					{data?.signed_contracts || 0}
+				</p>
+			</div>
 
-			<Card className="relative flex w-full max-w-72 flex-wrap items-start justify-between gap-6 p-4 before:absolute before:-left-px before:bottom-4 before:h-24 before:w-1 before:bg-foreground">
-				<div className="grid gap-2">
-					<h3 className="text-sm font-medium">Open roles</h3>
-					<p className="text-8xl font-bold">
-						{data?.contracts && data?.contracts < 10 ? '0' : ''}
-						{data?.contracts || 0}
-					</p>
-				</div>
-			</Card>
+			<div className="grid gap-2">
+				<h3 className="text-sm font-medium">Open roles</h3>
+				<p className="text-8xl font-bold">
+					{data?.contracts && data?.contracts < 10 ? '0' : ''}
+					{data?.contracts || 0}
+				</p>
+			</div>
 		</>
 	);
 };
