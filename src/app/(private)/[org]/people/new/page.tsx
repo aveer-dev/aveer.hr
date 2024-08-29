f71dfdc56087adfa504787bf2f92947748d2d86e
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2024-08-28 20:59:37 -0500

    improve contract form page performance

--- src/app/(private)/[org]/people/new/page.tsx
+++ src/app/(private)/[org]/people/new/page.tsx
@@ -4,6 +4,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { createClient } from '@/utils/supabase/server';
 import { TablesUpdate } from '@/type/database.types';
 import { BackButton } from '@/components/ui/back-button';
+import { PageLoader } from '@/components/ui/page-loader';
 
 export default async function Home({ params, searchParams }: { params: { [key: string]: string }; searchParams: { [key: string]: string } }) {
 	let contractDetails: TablesUpdate<'contracts'> = {};
@@ -17,24 +18,26 @@ export default async function Home({ params, searchParams }: { params: { [key: s
 	const { data } = await supabase.from('org_settings').select().eq('org', params.org).single();
 
 	return (
-		<div className="mx-auto max-w-4xl">
-			<div className="relative mb-6">
-				<BackButton className="absolute -left-16" />
+		<Suspense fallback={<PageLoader isLoading />}>
+			<div className="mx-auto max-w-4xl">
+				<div className="relative mb-6">
+					<BackButton className="absolute -left-16" />
 
-				<h1 className="text-xl font-semibold">Add person</h1>
+					<h1 className="text-xl font-semibold">Add person</h1>
+				</div>
+
+				<Suspense
+					fallback={
+						<div className="grid gap-6">
+							<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
+							<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
+							<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
+							<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
+						</div>
+					}>
+					<ContractForm orgBenefits={data} contractDuplicate={contractDetails} />
+				</Suspense>
 			</div>
-
-			<Suspense
-				fallback={
-					<div className="grid gap-6">
-						<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
-						<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
-						<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
-						<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
-					</div>
-				}>
-				<ContractForm orgBenefits={data} contractDuplicate={contractDetails} />
-			</Suspense>
-		</div>
+		</Suspense>
 	);
 }
