11690369ff9de19f9c87ff45a1169be12f3811e8
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2024-08-28 20:59:14 -0500

    update new role page with org settings data

--- src/app/(private)/[org]/open-roles/new/page.tsx
+++ src/app/(private)/[org]/open-roles/new/page.tsx
@@ -4,34 +4,39 @@ import { createClient } from '@/utils/supabase/server';
 import { TablesUpdate } from '@/type/database.types';
 import { BackButton } from '@/components/ui/back-button';
 import { ContractForm } from '@/components/forms/contract/form';
+import { PageLoader } from '@/components/ui/page-loader';
 
 export default async function Home({ params, searchParams }: { params: { [key: string]: string }; searchParams: { [key: string]: string } }) {
 	let roleDetails: TablesUpdate<'open_roles'> = {};
+	const supabase = createClient();
 
 	if (searchParams.duplicate) {
-		const supabase = createClient();
 		const { data } = await supabase.from('open_roles').select().match({ id: searchParams.duplicate, org: params.org }).single();
 		if (data) roleDetails = data;
 	}
 
-	return (
-		<div className="mx-auto max-w-4xl">
-			<div className="relative">
-				<BackButton className="absolute -left-16" />
-				<h1 className="mb-6 text-xl font-semibold">Create a role</h1>
-			</div>
+	const { data } = await supabase.from('org_settings').select().eq('org', params.org).single();
 
-			<Suspense
-				fallback={
-					<div className="grid gap-6">
-						<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
-						<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
-						<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
-						<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
-					</div>
-				}>
-				<ContractForm formType="role" openRoleDuplicate={roleDetails} />
-			</Suspense>
-		</div>
+	return (
+		<Suspense fallback={<PageLoader isLoading />}>
+			<div className="mx-auto max-w-4xl">
+				<div className="relative">
+					<BackButton className="absolute -left-16" />
+					<h1 className="mb-6 text-xl font-semibold">Create a role</h1>
+				</div>
+
+				<Suspense
+					fallback={
+						<div className="grid gap-6">
+							<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
+							<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
+							<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
+							<Skeleton className="h-60 w-full max-w-4xl"></Skeleton>
+						</div>
+					}>
+					<ContractForm orgBenefits={data} formType="role" openRoleDuplicate={roleDetails} />
+				</Suspense>
+			</div>
+		</Suspense>
 	);
 }
--- src/components/forms/contract/role.action.ts
+++ src/components/forms/contract/role.action.ts
@@ -2,15 +2,14 @@
 
 import { Database, TablesInsert, TablesUpdate } from '@/type/database.types';
 import { createClient } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
 
-export const createOpenRole = async (role: TablesInsert<'open_roles'>, orgId: string) => {
+export const createOpenRole = async (role: TablesInsert<'open_roles'>) => {
 	const supabase = createClient();
 
 	const { data, error } = await supabase.from('open_roles').insert(role).select('id').single();
 
 	if (error) return error.message;
-	if (!error) return redirect(`/${orgId}/open-roles/${data.id}`);
+	if (!error) return data.id;
 };
 
 export const updateRole = async (role: TablesUpdate<'open_roles'>, orgId: string, id: number) => {
@@ -28,6 +27,7 @@ export const toggleRoleStatus = async (status: Database['public']['Enums']['is_o
 	await supabase.from('open_roles').update({ state: status }).match({ id: role, org: org }).select();
 	return;
 };
+
 export const submitApplication = async (application: TablesInsert<'job_applications'>) => {
 	'use server';
 
