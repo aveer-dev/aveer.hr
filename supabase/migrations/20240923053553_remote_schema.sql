209c80167664dd6995b0494725d064fd7584bbc9
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2024-09-23 01:12:43 -0500

    storage schema

---
+++ supabase/migrations/20240923053553_remote_schema.sql
@@ -0,0 +1,67 @@
+grant insert on table "storage"."objects" to "PUBLIC";
+
+create policy "Allow public uploads to job-applications"
+on "storage"."objects"
+as permissive
+for insert
+to public
+with check ((bucket_id = 'job-applications'::text));
+
+
+create policy "Enable anyone to perform any action  1wwikoj_0"
+on "storage"."objects"
+as permissive
+for select
+to public
+using ((bucket_id = 'job-applications'::text));
+
+
+create policy "Enable anyone to perform any action  1wwikoj_1"
+on "storage"."objects"
+as permissive
+for update
+to public
+using ((bucket_id = 'job-applications'::text));
+
+
+create policy "Enable anyone to perform any action  1wwikoj_2"
+on "storage"."objects"
+as permissive
+for delete
+to public
+using ((bucket_id = 'job-applications'::text));
+
+
+create policy "Give users access to own folder flreew_0"
+on "storage"."objects"
+as permissive
+for insert
+to public
+with check (((bucket_id = 'documents'::text) AND ((( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[2]) OR (auth.role() = 'admin'::text))));
+
+
+create policy "Give users access to own folder flreew_1"
+on "storage"."objects"
+as permissive
+for update
+to public
+using (((bucket_id = 'documents'::text) AND ((( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[2]) OR (auth.role() = 'admin'::text))));
+
+
+create policy "Give users access to own folder flreew_2"
+on "storage"."objects"
+as permissive
+for delete
+to public
+using (((bucket_id = 'documents'::text) AND ((( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[2]) OR (auth.role() = 'admin'::text))));
+
+
+create policy "Give users access to own folder flreew_3"
+on "storage"."objects"
+as permissive
+for select
+to public
+using (((bucket_id = 'documents'::text) AND ((( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[2]) OR (auth.role() = 'admin'::text))));
+
+
+
