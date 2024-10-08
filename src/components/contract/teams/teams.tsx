a9ce253693fd3dce96be2d7e7bc1912af550fefa
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2024-10-07 19:04:11 -0500

    add direct reports to team component

--- src/components/contract/teams/teams.tsx
+++ src/components/contract/teams/teams.tsx
@@ -15,57 +15,123 @@ interface props {
 export const Teams = async ({ org, team, contractId, name, currentUser, isManager }: props) => {
 	const supabase = createClient();
 
-	const { data, error } = await supabase.from('contracts').select('*, level:employee_levels!contracts_level_fkey(*), profile:profiles!contracts_profile_fkey(*)').match({ org, team, status: 'signed' });
-	const { data: managers } = await supabase.from('managers').select().match({ team });
+	const [{ data, error }, { data: managers }, { data: directReports, error: directReportsError }] = await Promise.all([
+		await supabase.from('contracts').select('*, level:employee_levels!contracts_level_fkey(*), profile:profiles!contracts_profile_fkey(*)').match({ org, team, status: 'signed' }),
+		await supabase.from('managers').select().match({ team }),
+		await supabase.from('contracts').select('*, level:employee_levels!contracts_level_fkey(*), profile:profiles!contracts_profile_fkey(*)').match({ org, status: 'signed', direct_report: contractId })
+	]);
+
 	if (error) return error.message;
-	const filteredTeam = data.filter(contract => contract.id !== contractId);
+	const filteredTeam = data;
 
 	return (
-		<section className="mt-6 w-full">
-			<div className="mb-4">
-				<h2 className="flex items-center justify-between text-xl font-bold">Team</h2>
-				<p className="text-xs font-light text-muted-foreground">Team name: {name}</p>
-			</div>
-
-			{filteredTeam.length > 0 && (
-				<ul className="mt-6">
-					{filteredTeam.map(person => (
-						<li key={person.id} className="flex items-center justify-between border-t py-6">
-							<div className="space-y-2">
-								<div className="flex items-center gap-2">
-									<h2 className="text-xs">
-										{person.profile?.first_name} {person.profile?.last_name}
-									</h2>
-									{(person.level || person.level_name) && (
-										<Badge className="py-px text-[10px]" variant={'outline'}>
-											{person.level?.level || person.level_name}
-										</Badge>
-									)}
-									{!!managers?.find(manager => manager.person == person.id) && (
-										<Badge className="py-px text-[10px]" variant={'outline'}>
-											manager
-										</Badge>
-									)}
-								</div>
-								<p className="text-xs text-muted-foreground">{person.job_title}</p>
-							</div>
-
-							<div className="flex items-center gap-2">
-								{isManager && currentUser !== 'org' && <AppraisalsDialog role="manager" managerContract={contractId} org={org} contract={person} />}
-								<TeamMember person={person as any} />
-							</div>
-						</li>
-					))}
-
-					<Separator />
-				</ul>
-			)}
-
-			{filteredTeam.length == 0 && (
-				<div className="flex h-32 w-full items-center justify-center rounded-md bg-accent/80 text-xs text-muted-foreground">
-					<p>You do not have any team member</p>
+		<>
+			<section className="mt-6 w-full">
+				<div className="mb-4">
+					<h2 className="flex items-center justify-between text-xl font-bold">Team</h2>
+					<p className="text-xs font-light text-muted-foreground">Team name: {name}</p>
 				</div>
-			)}
-		</section>
+
+				{filteredTeam.length > 0 && (
+					<ul className="mt-6">
+						{filteredTeam.map(person => (
+							<li key={person.id} className="flex items-center justify-between border-t py-6">
+								<div className="space-y-2">
+									<div className="flex items-center gap-2">
+										<h2 className="text-xs">
+											{person.profile?.first_name} {person.profile?.last_name}
+										</h2>
+										{(person.level || person.level_name) && (
+											<Badge className="py-px text-[10px]" variant={'outline'}>
+												{person.level?.level || person.level_name}
+											</Badge>
+										)}
+										{!!managers?.find(manager => manager.person == person.id) && (
+											<Badge className="py-px text-[10px]" variant={'outline'}>
+												manager
+											</Badge>
+										)}
+									</div>
+									<p className="text-xs text-muted-foreground">{person.job_title}</p>
+								</div>
+
+								<div className="flex items-center gap-2">
+									{isManager && currentUser !== 'org' && <AppraisalsDialog role="manager" managerContract={contractId} org={org} contract={person} />}
+									<TeamMember person={person as any} />
+								</div>
+							</li>
+						))}
+
+						<Separator />
+					</ul>
+				)}
+
+				{filteredTeam.length == 0 && (
+					<div className="flex h-32 w-full items-center justify-center rounded-md bg-accent/80 text-xs text-muted-foreground">
+						<p>You do not have any team member</p>
+					</div>
+				)}
+			</section>
+
+			<section className="mt-16 w-full">
+				<div className="mb-4">
+					<h2 className="flex items-center justify-between text-xl font-bold">Direct reports</h2>
+					<p className="text-xs font-light text-muted-foreground">People reporting directly to you</p>
+				</div>
+
+				{(!directReports || directReportsError) && (
+					<div className="flex h-52 items-center justify-center rounded-md text-xs text-muted-foreground">
+						<p>{directReportsError.message || 'Unable to fetch direct reports'}</p>
+					</div>
+				)}
+
+				{!!directReports && (
+					<>
+						{directReports.length > 0 && (
+							<ul className="mt-6">
+								{directReports.map(person => (
+									<li key={person.id} className="flex items-center justify-between border-t py-6">
+										<div className="space-y-2">
+											<div className="flex items-center gap-2">
+												<h2 className="text-xs">
+													{person.profile?.first_name} {person.profile?.last_name}
+												</h2>
+
+												{(person.level || person.level_name) && (
+													<Badge className="py-px text-[10px]" variant={'outline'}>
+														{person.level?.level || person.level_name}
+													</Badge>
+												)}
+
+												{!!managers?.find(manager => manager.person == person.id) && (
+													<Badge className="py-px text-[10px]" variant={'outline'}>
+														manager
+													</Badge>
+												)}
+											</div>
+
+											<p className="text-xs text-muted-foreground">{person.job_title}</p>
+										</div>
+
+										<div className="flex items-center gap-2">
+											{isManager && currentUser !== 'org' && <AppraisalsDialog role="manager" managerContract={contractId} org={org} contract={person} />}
+											<TeamMember person={person as any} />
+										</div>
+									</li>
+								))}
+
+								<Separator />
+							</ul>
+						)}
+
+						{directReports.length == 0 && (
+							<div className="flex h-32 w-full items-center justify-center rounded-md bg-accent/80 text-xs text-muted-foreground">
+								<p>You do not have any direct reports</p>
+							</div>
+						)}
+					</>
+				)}
+			</section>
+		</>
 	);
 };
