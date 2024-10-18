9895e0646b7c76faa61bf64f6a40310d43be3f99
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2024-10-18 12:08:49 -0500

    auto redirect to first contract

--- src/app/(private)/(outside_org)/employee/page.tsx
+++ src/app/(private)/(outside_org)/employee/page.tsx
@@ -1,5 +1,29 @@
 import { PageLoader } from '@/components/ui/page-loader';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
 export default async function ContractorPage() {
+	const supabase = createClient();
+
+	const {
+		data: { user },
+		error: userError
+	} = await supabase.auth.getUser();
+
+	if (!user || userError) return redirect('/login');
+
+	const { data, error } = await supabase.from('contracts').select().eq('profile', user.id);
+
+	if (data && data.length) return redirect(`/employee/${data[0].org}/${data[0].id}/home`);
+
+	if (error || !data || !data.length) {
+		return (
+			<div className="flex min-h-56 flex-col items-center justify-center gap-2 bg-muted text-center text-muted-foreground">
+				<p>Unable to fetch contracts</p>
+				<p>{error?.message}</p>
+			</div>
+		);
+	}
+
 	return <PageLoader isLoading={true} />;
 }
