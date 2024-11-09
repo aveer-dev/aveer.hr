dd6094ce5035444a314c5a6ffd95cc61163fec9d
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2024-11-09 16:52:08 -0600

    improve org charge logic

--- src/app/(private)/[org]/org-chart/page.tsx
+++ src/app/(private)/[org]/org-chart/page.tsx
@@ -1,6 +1,6 @@
 import { createClient } from '@/utils/supabase/server';
 import FlowDiagram from './flow-diagram';
-import { Edge, Node } from '@xyflow/react';
+import { Edge, Node, Position } from '@xyflow/react';
 import { Tables } from '@/type/database.types';
 
 export default async function OrgChartPage({ params: { org } }: { params: { org: string } }) {
@@ -27,14 +27,29 @@ export default async function OrgChartPage({ params: { org } }: { params: { org:
 	}
 
 	const employees = people?.filter(person => !managers!.find(manager => manager.person === person.id));
-
 	const reportTos: { id: number; nodeId: string }[] = [];
+	const teamless = people?.filter(person => person.direct_report && !managers!.find(manager => manager.person === person.id) && !person.team);
 	people
 		?.filter(person => !!person.direct_report)
 		.map(person => (person.direct_report as any).id)
 		.forEach((reportTo: number) => (reportTos.find(rt => rt.id === reportTo) ? null : reportTos.push({ id: reportTo, nodeId: '' })));
 
-	const teamless = people?.filter(person => person.direct_report && !managers!.find(manager => manager.person === person.id) && !person.team);
+	// Utility function for adding a node
+	const addNode = ({ id, x, y, label, title, manager, type = 'custom', targetPosition }: { id: string; x: number; y: number; label: string; title: string; manager?: string[]; type?: string; targetPosition?: Position }): void => {
+		nodes.push({
+			id,
+			position: { x, y },
+			data: { label, title, manager },
+			width: 240,
+			type,
+			targetPosition
+		});
+	};
+
+	// Utility function for adding an edge
+	const addEdge = ({ id, source, target, animated = true, type = 'smoothstep' }: { id: string; source: string; target: string; animated?: boolean; type?: string }): void => {
+		edges.push({ id, source, target, animated, type, className: 'z-100' });
+	};
 
 	const nodes: Node[] = [];
 	const edges: Edge[] = [];
@@ -52,19 +67,8 @@ export default async function OrgChartPage({ params: { org } }: { params: { org:
 			const multipleTimesManager = managers?.filter(mn => (mn.person as any).id === (manager.person as any).id)?.map(mn => ({ id: mn.id, name: (mn.team as any).name }));
 
 			// creates manager node, also representing teams
-			if (multipleTimesManager!.findIndex(mn => mn.id === manager.id) === 0) {
-				nodes.push({
-					id: `MG${manager.id}`,
-					position: { x, y: 100 },
-					data: {
-						label: `${manager.profile?.first_name} ${manager.profile?.last_name}`,
-						title: `${(manager.person as any).job_title}`,
-						manager: multipleTimesManager.length > 1 ? multipleTimesManager.map(mn => mn.name) : [team.name]
-					},
-					width: 240,
-					type: 'custom'
-				});
-			}
+			if (multipleTimesManager!.findIndex(mn => mn.id === manager.id) === 0)
+				addNode({ id: `MG${manager.id}`, x, y: 100, title: `${(manager.person as any).job_title}`, label: `${manager.profile?.first_name} ${manager.profile?.last_name}`, manager: multipleTimesManager.length > 1 ? multipleTimesManager.map(mn => mn.name) : [team.name] });
 
 			const reportsTo: Tables<'contracts'> | null = (manager.person as unknown as Tables<'contracts'>)?.direct_report as any;
 
@@ -74,63 +78,20 @@ export default async function OrgChartPage({ params: { org } }: { params: { org:
 
 				if (reportTos[activeReportToIndex].nodeId == '') {
 					// Only create a new node if the person the manager reports to is not a manager
-					if (!isExistingManager) {
-						nodes.push({
-							id: `DR${reportsTo.id}`,
-							position: { x, y: 0 },
-							data: {
-								label: `${(reportsTo.profile as any)?.first_name} ${(reportsTo.profile as any)?.last_name}`,
-								title: `${reportsTo.job_title}`
-							},
-							type: 'custom',
-							width: 240
-						});
-					}
+					if (!isExistingManager) addNode({ id: `DR${reportsTo.id}`, x, y: -100, label: `${(reportsTo.profile as any)?.first_name} ${(reportsTo.profile as any)?.last_name}`, title: `${reportsTo.job_title}` });
 
 					reportTos[activeReportToIndex].nodeId = !!isExistingManager ? `MG${isExistingManager.id}` : `DR${reportsTo.id}`;
 				}
 
-				edges.push({
-					id: `MG-DR-e-${reportsTo.id}-${manager.id}`,
-					source: reportTos[activeReportToIndex].nodeId,
-					target: `MG${manager.id}`,
-					animated: true,
-					type: 'smoothstep'
-				});
+				addEdge({ id: `MG-DR-e-${reportsTo.id}-${manager.id}`, source: reportTos[activeReportToIndex].nodeId, target: `MG${manager.id}` });
 			}
 
 			teamMembers.forEach((member, idx) => {
 				if (member.id !== (manager.person as any).id) {
-					if (teamManagers!.findIndex(mn => mn.id === manager.id) === 0) {
-						nodes.push({
-							id: `EE${member.id}`,
-							position: { x, y: 400 + ((idx + 1) * 20 + idx * 50) },
-							data: {
-								label: `${member.profile?.first_name} ${member.profile?.last_name}`,
-								title: `${member.job_title}`
-							},
-							width: 200,
-							type: 'custom'
-						});
-					}
+					if (teamManagers!.findIndex(mn => mn.id === manager.id) === 0) addNode({ id: `EE${member.id}`, x, y: 400 + ((idx + 1) * 20 + idx * 50), label: `${member.profile?.first_name} ${member.profile?.last_name}`, title: `${member.job_title}` });
 
-					if (teamManagers!.findIndex(mn => mn.id === manager.id) === 0) {
-						edges.push({
-							id: `EE-e-${member.id}-${manager.id}`,
-							source: multipleTimesManager.length > 1 ? `MG${multipleTimesManager[0].id}` : `MG${manager.id}`,
-							target: `EE${member.id}`,
-							animated: true,
-							type: 'smoothstep'
-						});
-					} else {
-						edges.push({
-							id: `EE-e-${member.id}-${manager.id}`,
-							source: multipleTimesManager.length > 1 ? `MG${multipleTimesManager[0].id}` : `MG${teamManagers[0].id}`,
-							target: `EE${member.id}`,
-							animated: true,
-							type: 'smoothstep'
-						});
-					}
+					if (teamManagers!.findIndex(mn => mn.id === manager.id) === 0) addEdge({ id: `EE-e-${member.id}-${manager.id}`, source: multipleTimesManager.length > 1 ? `MG${multipleTimesManager[0].id}` : `MG${manager.id}`, target: `EE${member.id}` });
+					else addEdge({ id: `EE-e-${member.id}-${manager.id}`, source: multipleTimesManager.length > 1 ? `MG${multipleTimesManager[0].id}` : `MG${teamManagers[0].id}`, target: `EE${member.id}` });
 				}
 
 				// a team member might have someone they report to, other than the team manager
@@ -144,45 +105,19 @@ export default async function OrgChartPage({ params: { org } }: { params: { org:
 
 					if (reportTos[activeReportToIndex].nodeId == '') {
 						// Only create a new node if the person the manager reports to is not a manager
-						if (!isExistingManager) {
-							nodes.push({
-								id: `DR${reportsTo.id}`,
-								position: { x, y: 0 },
-								data: {
-									label: `${(reportsTo.profile as any)?.first_name} ${(reportsTo.profile as any)?.last_name}`,
-									title: `${reportsTo.job_title}`
-								},
-								width: 240,
-								type: 'custom'
-							});
-						}
+						if (!isExistingManager) addNode({ id: `DR${reportsTo.id}`, x, y: 0, label: `${(reportsTo.profile as any)?.first_name} ${(reportsTo.profile as any)?.last_name}`, title: `${reportsTo.job_title}` });
 
 						reportTos[activeReportToIndex].nodeId = !!isExistingManager ? `MG${isExistingManager.id}` : `DR${reportsTo.id}`;
 					}
 
-					edges.push({
-						id: `EE-DR-e-${reportsTo.id}-${member.id}`,
-						source: reportTos[activeReportToIndex].nodeId,
-						target: `EE${member.id}`,
-						animated: true,
-						type: 'smoothstep'
-					});
+					addEdge({ id: `EE-DR-e-${reportsTo.id}-${member.id}`, source: reportTos[activeReportToIndex].nodeId, target: `EE${member.id}` });
 				}
 			});
 		});
 	}
 
 	teamless!.forEach(person => {
-		nodes.push({
-			id: `TL${person.id}`,
-			position: { x: 200 + teams!.length * 300, y: 100 },
-			data: {
-				label: `${person.profile?.first_name} ${person.profile?.last_name}`,
-				title: `${person.job_title}`
-			},
-			width: 240,
-			type: 'custom'
-		});
+		addNode({ id: `TL${person.id}`, x: 200 + teams!.length * 300, y: 100, label: `${person.profile?.first_name} ${person.profile?.last_name}`, title: `${person.job_title}` });
 
 		const reportsTo: Tables<'contracts'> | null = person?.direct_report as any;
 
@@ -191,39 +126,22 @@ export default async function OrgChartPage({ params: { org } }: { params: { org:
 			const activeReportToIndex = reportTos.findIndex(rt => rt.id === reportsTo.id);
 
 			if (reportTos[activeReportToIndex].nodeId == '') {
-				if (!isExistingManager) {
-					nodes.push({
-						id: `DR${reportsTo.id}`,
-						position: { x: 200 + teams!.length * 300, y: 0 },
-						data: {
-							label: `${(reportsTo.profile as any)?.first_name} ${(reportsTo.profile as any)?.last_name}`,
-							title: `${reportsTo.job_title}`
-						},
-						width: 240,
-						type: 'custom'
-					});
-				}
+				if (!isExistingManager) addNode({ id: `DR${reportsTo.id}`, x: 200 + teams!.length * 300, y: 0, label: `${(reportsTo.profile as any)?.first_name} ${(reportsTo.profile as any)?.last_name}`, title: `${reportsTo.job_title}` });
 
 				reportTos[activeReportToIndex].nodeId = isExistingManager ? `MG${isExistingManager.id}` : `DR${reportsTo.id}`;
 			}
 
-			edges.push({
-				id: `TL-DR-e-${reportsTo.id}-${person.id}`,
-				source: reportTos[activeReportToIndex].nodeId,
-				target: `TL${person.id}`,
-				animated: true,
-				type: 'smoothstep'
-			});
+			addEdge({ id: `TL-DR-e-${reportsTo.id}-${person.id}`, source: reportTos[activeReportToIndex].nodeId, target: `TL${person.id}` });
 		}
 	});
 
 	return (
-		<div className="container mx-auto space-y-8 p-0">
-			<div className="w-full items-center gap-6">
+		<div className="absolute bottom-0 left-0 right-0 top-32 mx-auto space-y-8 p-0">
+			{/* <div className="w-full items-center gap-6">
 				<h1 className="text-2xl font-medium">Organisation Chart</h1>
-			</div>
+			</div> */}
 
-			<div className="h-[70vh] w-full">
+			<div className="h-[85vh] w-full">
 				<FlowDiagram defaultNodes={nodes} edges={edges} />
 			</div>
 		</div>
