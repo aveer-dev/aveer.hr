f42ecca92f3dc33b97b07e6ee97e73d5359aed78
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2024-12-19 03:33:15 -0600

    db migration

---
+++ supabase/migrations/20241219092920_plane_integration.sql
@@ -0,0 +1,9 @@
+alter table "public"."org_settings" add column "enable_task_manager" boolean not null default false;
+
+alter table "public"."org_settings" add column "plane_key" text;
+
+alter table "public"."org_settings" add column "plane_project" text;
+
+alter table "public"."org_settings" add column "plane_workspace_slug" text;
+
+
