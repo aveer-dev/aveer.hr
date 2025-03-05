67c9806ed38666f8f6eeee79c8b604dfa98c69cb
horlah <emmanuel.ainaj@gmail.com>
2025-03-05 16:51:23 -0600

    only show todos if there is a todo

--- src/app/employee/[org]/[contract]/home/todos.tsx
+++ src/app/employee/[org]/[contract]/home/todos.tsx
@@ -21,42 +21,44 @@ export const Todos = async ({ profile, contract, org, team }: { profileId?: stri
 	const todos = [...(leaveRequests ? leaveRequests : []), ...(applicants ? applicants : [])].sort((a, b) => (new Date((a as any)?.created_at) as any) - (new Date((b as any)?.created_at) as any));
 
 	return (
-		<section className={cn(contract.status !== 'signed' && 'pointer-events-none opacity-50 blur-sm', 'w-full')}>
-			<h2 className="mb-4 text-sm font-normal text-support">Todos</h2>
+		!!todos.length && (
+			<section className={cn(contract.status !== 'signed' && 'pointer-events-none opacity-50 blur-sm', 'w-full')}>
+				<h2 className="mb-4 text-sm font-normal text-support">Todos</h2>
 
-			<ul className="space-y-2 rounded-md pb-4 text-sm text-primary/90">
-				{todos.map((item, index) => {
-					if (typeof item == 'string') return;
-					return (
-						<li key={index} className="flex items-center gap-2">
-							<div className="h-1 w-1 rounded-full bg-primary/40"></div>
+				<ul className="space-y-2 rounded-md pb-4 text-sm text-primary/90">
+					{todos.map((item, index) => {
+						if (typeof item == 'string') return;
+						return (
+							<li key={index} className="flex items-center gap-2">
+								<div className="h-1 w-1 rounded-full bg-primary/40"></div>
 
-							{!!(item as any)?.from && !!(item as any)?.to && (
-								<LeaveReview reviewType={'manager'} data={item as any} contract={(item as any)?.contract}>
-									<Button variant={'ghost'} className="block h-[unset] w-[unset] gap-3 whitespace-normal py-3 text-left">
+								{!!(item as any)?.from && !!(item as any)?.to && (
+									<LeaveReview reviewType={'manager'} data={item as any} contract={(item as any)?.contract}>
+										<Button variant={'ghost'} className="block h-[unset] w-[unset] gap-3 whitespace-normal py-3 text-left">
+											<time className="pr-2 text-muted-foreground">{format(item.created_at as string, 'PPP')}</time>
+											<span className="">
+												{(item as any)?.profile?.first_name} has a {(item as any)?.leave_type} leave request pending your approval
+											</span>
+										</Button>
+									</LeaveReview>
+								)}
+
+								{(item as any)?.role && (
+									<ApplicantDetails
+										key={index}
+										userRole={manager || (item as any)?.role.direct_report == contract.id ? 'manager' : 'employee'}
+										contractId={contract.id}
+										data={item as any}
+										className={cn(buttonVariants({ variant: 'ghost' }), 'block whitespace-normal py-3 text-left text-xs font-light')}>
 										<time className="pr-2 text-muted-foreground">{format(item.created_at as string, 'PPP')}</time>
-										<span className="">
-											{(item as any)?.profile?.first_name} has a {(item as any)?.leave_type} leave request pending your approval
-										</span>
-									</Button>
-								</LeaveReview>
-							)}
-
-							{(item as any)?.role && (
-								<ApplicantDetails
-									key={index}
-									userRole={manager || (item as any)?.role.direct_report == contract.id ? 'manager' : 'employee'}
-									contractId={contract.id}
-									data={item as any}
-									className={cn(buttonVariants({ variant: 'ghost' }), 'block whitespace-normal py-3 text-left text-xs font-light')}>
-									<time className="pr-2 text-muted-foreground">{format(item.created_at as string, 'PPP')}</time>
-									<span className="">You have a {(item as any)?.role.job_title} role applicant pending your review</span>
-								</ApplicantDetails>
-							)}
-						</li>
-					);
-				})}
-			</ul>
-		</section>
+										<span className="">You have a {(item as any)?.role.job_title} role applicant pending your review</span>
+									</ApplicantDetails>
+								)}
+							</li>
+						);
+					})}
+				</ul>
+			</section>
+		)
 	);
 };
