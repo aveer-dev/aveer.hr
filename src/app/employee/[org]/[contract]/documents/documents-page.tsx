884772e60d0643d61d4f617b5a0d738dfdeac439
horlah <emmanuel.ainaj@gmail.com>
2025-05-22 16:08:48 -0700

    Refactor DocumentsPage component to streamline document creation logic
    - Removed the createDocument function from DocumentsPage, simplifying the component structure.
    - Updated DocumentsList to accept org as a prop, enhancing document filtering capabilities based on organization.

--- src/app/employee/[org]/[contract]/documents/documents-page.tsx
+++ src/app/employee/[org]/[contract]/documents/documents-page.tsx
@@ -17,20 +17,7 @@ export const DocumentsPage = async ({ org, contract }: { org: string; contract: 
 
 	if (!user) return redirect('/app/login');
 
-	const createDocument = async () => {
-		'use server';
-
-		const supabase = await createClient();
-
-		const res = await supabase
-			.from('documents')
-			.insert({ org, shared_with: [{ profile: user.id, access: 'owner', contract: contract }], name: `New Document - ${format(new Date(), 'PPP')}` })
-			.select()
-			.single();
-		return res;
-	};
-
 	const documents = docs.filter(document => document.shared_with?.filter((person: any) => person.contract == contract) || document.owner == user.id);
 
-	return <DocumentsList currentUserId={user.id} documents={documents} createDocument={createDocument} />;
+	return <DocumentsList currentUserId={user.id} documents={documents} org={org} />;
 };
